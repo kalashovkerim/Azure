bb47a577753695214963cb04d730a9bac7327acb
Karim Kalashov <kerim.kalashov@gmail.com>
2022-12-19 20:15:44 +0400

    first commit

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NimbApp.Models;
+namespace NimbApp.Controllers
+{
+    public class AuthController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Login([Bind("UserName,Password")] AuthViewModel authModel)
+        {
+            if (ModelState.IsValid)
+            {
+                if(authModel.UserName == "admin" && authModel.Password == "admin")
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+                return View();
+            }
+            return View();
+        }
+    }
+}
---
+++ Models/AuthViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NimbApp.Models
+{
+    public class AuthViewModel
+    {
+        [Required]
+        public string? UserName { get; set; }
+        [Required]
+        public string? Password { get; set; } 
+    }
+}
