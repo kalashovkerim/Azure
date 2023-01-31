7c8b4a1d73a195e728f6806bd253f4fae9f3625e
kalashovkerim <kkalashov.unity@gmail.com>
2023-01-31 11:17:46 +0400

    Password and Login Generators

---
+++ Business/Services/Classes/GenerateUserLogin.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Services.Classes
+{
+    public class GenerateUserLogin
+    {
+        public static string FromName(string _name)
+        {
+            string login = "";
+            var random = new Random();
+
+            for (int i = 0; i < _name.Length / 2; i++)
+            {
+                login += _name[i];
+            }
+            
+            for (int i = 0; i < 4; i++)
+            {
+                int num = random.Next(0, 10);
+                login += num.ToString();
+            }
+            return login; 
+        }
+    }
+}
---
+++ Business/Services/Classes/GenerateUserPassword.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Services.Classes
+{
+    public class GenerateUserPassword
+    {
+        private static string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*";
+        public static string Generate()
+        {
+            var rand = new Random();
+            string password = "";
+            
+            
+            int length = rand.Next(4,6);
+            for (int i = 0; i < length; i++)
+            {
+                password += chars[rand.Next(0, chars.Length)];
+            }
+
+            return password;
+        }
+    }
+}
